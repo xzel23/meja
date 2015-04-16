b95e2fd6f84f37cfb52cd433ba4a839c3e55aba7
Axel Howind <axel@dua3.com>
2015-04-16 10:12:18 +0200

    Fix exception on workbook save.

--- meja/src/com/dua3/meja/model/poi/PoiWorkbookFactory.java
+++ meja/src/com/dua3/meja/model/poi/PoiWorkbookFactory.java
@@ -28,6 +28,7 @@ import com.dua3.meja.model.Workbook;
 import com.dua3.meja.model.WorkbookFactory;
 import com.dua3.meja.model.poi.PoiWorkbook.PoiHssfWorkbook;
 import com.dua3.meja.model.poi.PoiWorkbook.PoiXssfWorkbook;
+import java.io.FileInputStream;
 
 /**
  *
@@ -35,99 +36,96 @@ import com.dua3.meja.model.poi.PoiWorkbook.PoiXssfWorkbook;
  */
 public final class PoiWorkbookFactory extends WorkbookFactory {
 
-	private static final PoiWorkbookFactory INSTANCE = new PoiWorkbookFactory();
+    private static final PoiWorkbookFactory INSTANCE = new PoiWorkbookFactory();
 
-	public static PoiWorkbookFactory instance() {
-		return INSTANCE;
-	}
+    public static PoiWorkbookFactory instance() {
+        return INSTANCE;
+    }
 
-	private PoiWorkbookFactory() {
-	}
+    private PoiWorkbookFactory() {
+    }
 
-	@Override
-	public Workbook open(File file) throws IOException {
-		try {
-			Locale locale = Locale.getDefault();
-			final org.apache.poi.ss.usermodel.Workbook poiWorkbook = org.apache.poi.ss.usermodel.WorkbookFactory
-					.create(file);
-			return createWorkbook(poiWorkbook, locale);
-		} catch (org.apache.poi.openxml4j.exceptions.InvalidFormatException ex) {
-			throw new FileFormatException(ex.getMessage());
-		}
-	}
+    @Override
+    public Workbook open(File file) throws IOException {
+        // Do not use the create(File) method to avoid exception when trying to 
+        // save the workbook again. 
+        try (InputStream in = new FileInputStream(file)) {            
+            return open(in, Locale.getDefault());
+        }
+    }
 
-	public Workbook open(InputStream in)
-			throws IOException {
-		return open(in, Locale.getDefault());
-	}
+    public Workbook open(InputStream in)
+            throws IOException {
+        return open(in, Locale.getDefault());
+    }
 
-	public Workbook open(InputStream in, Locale locale)
-			throws IOException {
-		try {
-			final org.apache.poi.ss.usermodel.Workbook poiWorkbook = org.apache.poi.ss.usermodel.WorkbookFactory
-					.create(in);
-			return createWorkbook(poiWorkbook, locale);
-		} catch (org.apache.poi.openxml4j.exceptions.InvalidFormatException ex) {
-			throw new FileFormatException(ex.getMessage());
-		}
-	}
+    public Workbook open(InputStream in, Locale locale)
+            throws IOException {
+        try {
+            final org.apache.poi.ss.usermodel.Workbook poiWorkbook = org.apache.poi.ss.usermodel.WorkbookFactory
+                    .create(in);
+            return createWorkbook(poiWorkbook, locale);
+        } catch (org.apache.poi.openxml4j.exceptions.InvalidFormatException ex) {
+            throw new FileFormatException(ex.getMessage());
+        }
+    }
 
-	public Workbook open(URI uri) throws IOException {
-		return open(uri, Locale.getDefault());
-	}
+    public Workbook open(URI uri) throws IOException {
+        return open(uri, Locale.getDefault());
+    }
 
-	public Workbook open(URI uri, Locale locale)
-			throws IOException {
-		return open(uri.toURL(), locale);
-	}
+    public Workbook open(URI uri, Locale locale)
+            throws IOException {
+        return open(uri.toURL(), locale);
+    }
 
-	public Workbook open(URL url) throws IOException {
-		return open(url, Locale.getDefault());
-	}
+    public Workbook open(URL url) throws IOException {
+        return open(url, Locale.getDefault());
+    }
 
-	public Workbook open(URL url, Locale locale)
-			throws IOException {
-		try (InputStream in = new BufferedInputStream(url.openStream())) {
-			return open(in, locale);
-		}
-	}
+    public Workbook open(URL url, Locale locale)
+            throws IOException {
+        try (InputStream in = new BufferedInputStream(url.openStream())) {
+            return open(in, locale);
+        }
+    }
 
-	@Override
-	public Workbook create() {
-		return createXls(Locale.getDefault());
-	}
+    @Override
+    public Workbook create() {
+        return createXls(Locale.getDefault());
+    }
 
-	public Workbook createXls() {
-		return createXls(Locale.getDefault());
-	}
+    public Workbook createXls() {
+        return createXls(Locale.getDefault());
+    }
 
-	public Workbook createXls(Locale locale) {
-		return new PoiHssfWorkbook(
-				new org.apache.poi.hssf.usermodel.HSSFWorkbook(), locale);
-	}
+    public Workbook createXls(Locale locale) {
+        return new PoiHssfWorkbook(
+                new org.apache.poi.hssf.usermodel.HSSFWorkbook(), locale);
+    }
 
-	public Workbook createXlsx() {
-		return createXlsx(Locale.getDefault());
-	}
+    public Workbook createXlsx() {
+        return createXlsx(Locale.getDefault());
+    }
 
-	public Workbook createXlsx(Locale locale) {
-		return new PoiXssfWorkbook(
-				new org.apache.poi.xssf.usermodel.XSSFWorkbook(), locale);
-	}
+    public Workbook createXlsx(Locale locale) {
+        return new PoiXssfWorkbook(
+                new org.apache.poi.xssf.usermodel.XSSFWorkbook(), locale);
+    }
 
-	private Workbook createWorkbook(
-			final org.apache.poi.ss.usermodel.Workbook poiWorkbook, Locale locale) {
-		if (poiWorkbook instanceof org.apache.poi.hssf.usermodel.HSSFWorkbook) {
-			return new PoiHssfWorkbook(
-					(org.apache.poi.hssf.usermodel.HSSFWorkbook) poiWorkbook,
-					locale);
-		} else if (poiWorkbook instanceof org.apache.poi.xssf.usermodel.XSSFWorkbook) {
-			return new PoiXssfWorkbook(
-					(org.apache.poi.xssf.usermodel.XSSFWorkbook) poiWorkbook,
-					locale);
-		} else {
-			throw new IllegalStateException();
-		}
-	}
+    private Workbook createWorkbook(
+            final org.apache.poi.ss.usermodel.Workbook poiWorkbook, Locale locale) {
+        if (poiWorkbook instanceof org.apache.poi.hssf.usermodel.HSSFWorkbook) {
+            return new PoiHssfWorkbook(
+                    (org.apache.poi.hssf.usermodel.HSSFWorkbook) poiWorkbook,
+                    locale);
+        } else if (poiWorkbook instanceof org.apache.poi.xssf.usermodel.XSSFWorkbook) {
+            return new PoiXssfWorkbook(
+                    (org.apache.poi.xssf.usermodel.XSSFWorkbook) poiWorkbook,
+                    locale);
+        } else {
+            throw new IllegalStateException();
+        }
+    }
 
 }
