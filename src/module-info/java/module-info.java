1011a99b6aa0e4ad84a9bdd7c5ede55749376b2c
Axel Howind <axel@dua3.com>
2018-03-12 10:44:20 +0100

    preparing Java 9 Jigsaw support

---
+++ src/module-info/java/module-info.java
@@ -0,0 +1,8 @@
+module com.dua3.meja {
+    exports com.dua3.meja.io;
+    exports com.dua3.meja.model;
+    exports com.dua3.meja.util;
+    exports com.dua3.meja.ui;
+
+    requires org.apache.logging.log4j;
+}
