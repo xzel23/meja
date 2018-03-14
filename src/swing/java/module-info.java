ec76e502729a0afdb032152cdef01443f1eea8ef
Axel Howind <axel@dua3.com>
2018-03-14 09:50:52 +0100

    correct entries in module-info.java

--- src/swing/java/module-info.java
+++ src/swing/java/module-info.java
@@ -1,10 +1,7 @@
-module com.dua3.meja {
+module com.dua3.meja.ui.swing {
     exports com.dua3.meja.ui.swing;
 
-    requires com.dua3.meja.io;
-    requires com.dua3.meja.model;
-    requires com.dua3.meja.util;
-    requires com.dua3.meja.ui;
+    requires com.dua3.meja;
 
     requires org.apache.logging.log4j;
     requires com.dua3.utility;
