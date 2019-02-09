42b5413e226e7f5a7a4e6a0a17051f08cf725e31
Axel Howind <axel@dua3.com>
2019-02-09 09:49:58 +0100

    gitlab ci

--- update-ghpages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./gradlew dist || exit
-cp -r doc /tmp/meja-javadoc
-git checkout gh-pages
-rm -r javadoc
-mv /tmp/meja-javadoc ./javadoc
-git add javadoc
-git commit -m "updated javadoc"
