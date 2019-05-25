46ab5b7a409811b0c88e6432013803a029a343e8
Axel Howind <axel@dua3.com>
2019-05-25 21:59:18 +0200

    new DbMejaUtil utility class

---
+++ meja-db/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.dua3.meja.db {
+    exports com.dua3.meja.db;
+
+    requires transitive com.dua3.meja;
+
+    requires java.logging;
+    requires java.sql;
+    requires com.dua3.utility;
+}
