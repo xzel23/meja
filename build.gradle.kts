474d6d8138804e52f3848bbad12b6499ab90f533
Axel Howind <axel@dua3.com>
2022-08-15 11:08:18 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ object meta {
 val isReleaseVersion = !meta.version.endsWith("SNAPSHOT")
 
 val javafxVersion       by extra { "18.0.2" }
-val dua3UtilityVersion  by extra { "10.0.0" }
+val dua3UtilityVersion  by extra { "10.0.1-SNAPSHOT" }
 
 subprojects {
 
