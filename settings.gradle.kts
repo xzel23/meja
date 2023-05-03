44b62906ddba69f9c57503c877820fbbea99493a
Axel Howind <axh@dua3.com>
2023-05-03 13:30:50 +0200

    bump version; update utility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-meja"
-val projectVersion = "4.0.0-M5"
+val projectVersion = "4.0.0-M6-SNAPSHOT"
 
 include("meja")
 include("meja-generic")
@@ -24,7 +24,7 @@ dependencyResolutionManagement {
             plugin("javafx", "org.openjfx.javafxplugin").version("0.0.13")
 
             version("cabe", "1.0.0")
-            version("dua3-utility", "11.0.0-M3")
+            version("dua3-utility", "11.0.0-M5")
             version("javafx", "20")
             version("junit", "5.9.2")
             version("log4j", "2.20.0")
