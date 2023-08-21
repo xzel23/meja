b8d1f4499d863567c602815b803d0d1677c160a1
Axel Howind <axh@dua3.com>
2023-08-21 15:38:58 +0200

    start v4.0.0-M10 development cycle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-meja"
-val projectVersion = "4.0.0-M9"
+val projectVersion = "4.0.0-M10-SNAPSHOT"
 
 include("meja")
 include("meja-generic")
@@ -19,12 +19,12 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.47.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
-            plugin("spotbugs", "com.github.spotbugs").version("5.1.2")
+            plugin("spotbugs", "com.github.spotbugs").version("5.1.3")
             plugin("cabe", "com.dua3.cabe").version("1.2.0")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.0.14")
 
             version("cabe", "1.0.0")
-            version("dua3-utility", "11.0.0-M9")
+            version("dua3-utility", "11.0.0-M11")
             version("javafx", "20")
             version("junit", "5.10.0")
             version("log4j", "2.20.0")
