3766be77da7e2154762cfcf5d6c52d51cd33a2f4
Axel Howind <axh@dua3.com>
2023-09-11 07:49:30 +0200

    start 4.0.1 cycle, update plugins and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "dua3-meja"
-val projectVersion = "4.0.0"
+val projectVersion = "4.0.1-SNAPSHOT"
 
 include("meja")
 include("meja-generic")
@@ -17,11 +17,11 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.47.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.48.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
             plugin("spotbugs", "com.github.spotbugs").version("5.1.3")
-            plugin("cabe", "com.dua3.cabe").version("1.2.0")
-            plugin("javafx", "org.openjfx.javafxplugin").version("0.0.14")
+            plugin("cabe", "com.dua3.cabe").version("1.3.0")
+            plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "1.0.0")
             version("dua3-utility", "11.0.0")
@@ -29,7 +29,7 @@ dependencyResolutionManagement {
             version("junit", "5.10.0")
             version("log4j", "2.20.0")
             version("poi", "5.2.3")
-            version("slf4j", "2.0.7")
+            version("slf4j", "2.0.9")
 
             library("cabe-annotations", "com.dua3.cabe", "cabe-annotations").versionRef("cabe")
             library("dua3-utility", "com.dua3.utility", "utility").versionRef("dua3-utility")
