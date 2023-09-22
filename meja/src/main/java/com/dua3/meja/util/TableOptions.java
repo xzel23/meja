425f9ea50e0656f377f3791bf714054766ac396e
Axel Howind <axh@dua3.com>
2023-09-22 08:40:00 +0200

    clean Javadoc

--- meja/src/main/java/com/dua3/meja/util/TableOptions.java
+++ meja/src/main/java/com/dua3/meja/util/TableOptions.java
@@ -3,7 +3,7 @@ package com.dua3.meja.util;
 public enum TableOptions {
     /**
      * Indicates whether the first row in a table is a header row.
-     *
+     * <p>
      * If the value is set to true, it means that the first row of the table contains
      * header information, such as column names. If set to false, the first row is treated
      * as a regular data row.
