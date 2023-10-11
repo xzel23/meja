77eec1fbe3f8389a43ce35e1599feabf95e337e0
Axel Howind <axh@dua3.com>
2023-10-11 07:01:24 +0200

    update versions, spotbugs plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,9 +17,9 @@ dependencyResolutionManagement {
         create("libs") {
             version("projectVersion", projectVersion)
 
-            plugin("versions", "com.github.ben-manes.versions").version("0.48.0")
+            plugin("versions", "com.github.ben-manes.versions").version("0.49.0")
             plugin("test-logger", "com.adarshr.test-logger").version("3.2.0")
-            plugin("spotbugs", "com.github.spotbugs").version("5.1.3")
+            plugin("spotbugs", "com.github.spotbugs").version("5.1.4")
             plugin("cabe", "com.dua3.cabe").version("1.3.0")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
