89a82650851d5fcd08b6089798c6dd10eeb7e368
Axel Howind <axh@dua3.com>
2024-01-09 09:13:15 +0100

    conupdate utility to 12.0.0 final; update spotbugs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,12 +27,12 @@ dependencyResolutionManagement {
 
             plugin("versions", "com.github.ben-manes.versions").version("0.50.0")
             plugin("test-logger", "com.adarshr.test-logger").version("4.0.0")
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.5")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.6")
             plugin("cabe", "com.dua3.cabe").version("2.0")
             plugin("javafx", "org.openjfx.javafxplugin").version("0.1.0")
 
             version("cabe", "2.0")
-            version("dua3-utility", "12.0.0-beta12")
+            version("dua3-utility", "12.0.0")
             version("javafx", "21.0.1")
             version("junit", "5.10.1")
             version("log4j", "2.22.1")
