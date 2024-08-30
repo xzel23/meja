4db8395885ee70462eb9eeb5d2665efd20c08b4d
Axel Howind <axh@dua3.com>
2024-08-30 22:46:01 +0200

    v5.0-RC1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 // define project name and version
 rootProject.name = "dua3-meja"
-val projectVersion = "5.0-BETA4-SNAPSHOT"
+val projectVersion = "5.0-RC1"
 
 // define subprojects
 include("meja")
