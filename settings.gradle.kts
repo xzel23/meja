8933cb8c9839bc1ffc2a94992d370c958769cb4e
Axel Howind <axh@dua3.com>
2025-02-12 16:54:29 +0100

    update utility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
             plugin("spotbugs", "com.github.spotbugs").version("6.1.3")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
 
-            version("dua3-utility", "16.0.1-SNAPSHOT")
+            version("dua3-utility", "16.0.2-SNAPSHOT")
             version("dua3-fx", "1.0.4")
             version("ikonli", "12.3.1")
             version("jspecify", "1.0.0")
