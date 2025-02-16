7df35e9f3a5879bc9559189dd1f0cc3108ad8b7f
Axel Howind <axh@dua3.com>
2025-02-16 22:41:05 +0100

    v7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-meja"
-val projectVersion = "7-RC"
+val projectVersion = "7"
 
 // define subprojects
 include("meja")
@@ -30,8 +30,8 @@ dependencyResolutionManagement {
             plugin("spotbugs", "com.github.spotbugs").version("6.1.4")
             plugin("cabe", "com.dua3.cabe").version("3.0.2")
 
-            version("dua3-utility", "16.0.2")
-            version("dua3-fx", "1.0.4")
+            version("dua3-utility", "16.1")
+            version("dua3-fx", "1.0.5")
             version("ikonli", "12.3.1")
             version("jspecify", "1.0.0")
             version("log4j-bom", "2.24.3")
