fe14066260f9b72cf33914f76c6f0af482bbc229
Axel Howind <axh@dua3.com>
2025-04-30 07:31:49 +0200

    v8.0.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 // define project name and version
 rootProject.name = "dua3-meja"
-val projectVersion = "8.0.4-SNAPSHOT"
+val projectVersion = "8.0.4"
 
 // define subprojects
 include("meja")
