918bb22aa4ecb931ff5de0f643ade4f4e4f8a38c
Axel Howind <axh@dua3.com>
2025-07-07 06:54:35 +0200

    remove jvm-test-suite plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -117,7 +117,6 @@ subprojects {
     if (!project.name.endsWith("-bom")) {
         apply(plugin = "jacoco")
         apply(plugin = "java-library")
-        apply(plugin = "jvm-test-suite")
         apply(plugin = rootProject.libs.plugins.spotbugs.get().pluginId)
         apply(plugin = rootProject.libs.plugins.cabe.get().pluginId)
         apply(plugin = rootProject.libs.plugins.forbiddenapis.get().pluginId)
