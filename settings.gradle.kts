2a0477b9b873d2e11bf8b712890140290001e219
Axel Howind <axh@dua3.com>
2025-08-14 10:04:20 +0200

    v9.0.0-beta4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-meja"
-val projectVersion = "9.0.0-beta4-SNAPSHOT"
+val projectVersion = "9.0.0-beta4"
 
 include("meja-core")
 include("meja-generic")
