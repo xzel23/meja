58f543dc5a64a55e8a299b9e9a979c673113a7b0
Axel Howind <axh@dua3.com>
2025-09-16 19:26:22 +0200

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-meja"
-val projectVersion = "9.0.0-beta7"
+val projectVersion = "9.0.0-beta8-SNAPSHOT"
 
 include("meja-bom")
 include("meja-core")
