2496895b0630ea27e459202aaadfa6b0d5821e33
Axel Howind <axh@dua3.com>
2025-11-03 14:21:06 +0100

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,7 +37,7 @@ dependencyResolutionManagement {
             plugin("jmh", "me.champeau.jmh").version("0.7.3")
             plugin("jreleaser", "org.jreleaser").version("1.21.0")
 
-            version("dua3-utility", "20.0.0-rc-4")
+            version("dua3-utility", "20.0.0-rc-5")
             version("ikonli", "12.4.0")
             version("javafx", "23.0.2")
             version("jmh", "1.37")
