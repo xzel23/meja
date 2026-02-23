f630a272dfee1a1fba69ab87bd1b7c322fda8036
Axel Howind <axh@dua3.com>
2026-02-23 04:34:32 +0100

    Bump version to 10.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.internal.extensions.stdlib.toDefaultLowerCase
 
 rootProject.name = "dua3-meja"
-val projectVersion = "10.1.0-rc"
+val projectVersion = "10.1.0"
 
 include("meja-bom")
 include("meja-core")
