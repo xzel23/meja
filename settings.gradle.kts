06af3751f2c67b6195da40c2554edb04ed10aa9e
Axel Howind <axh@dua3.com>
2026-09-14 10:14:24 +0200

    Update versions, plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -50,7 +50,7 @@ private fun readReleaseVersions(file: File, requireSelection: Boolean): ReleaseV
 }
 
 plugins {
-    id("io.github.ben-manes.versions.settings") version "0.61.0"
+    id("io.github.ben-manes.versions.settings") version "0.62.0"
 }
 
 rootProject.name = "dua3-meja"
